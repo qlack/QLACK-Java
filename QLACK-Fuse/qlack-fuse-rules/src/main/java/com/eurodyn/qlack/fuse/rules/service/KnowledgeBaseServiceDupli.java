de956ebea2426cff9f4bbc68871c4bafc15e60ba
TMargaret <tmargaret@hotmail.com>
2019-09-17 10:31:17 +0300

    test on duplicated code

---
+++ QLACK-Fuse/qlack-fuse-rules/src/main/java/com/eurodyn/qlack/fuse/rules/service/KnowledgeBaseServiceDupli.java
@@ -0,0 +1,115 @@
+package com.eurodyn.qlack.fuse.rules.service;
+
+import com.eurodyn.qlack.common.exception.QDoesNotExistException;
+import com.eurodyn.qlack.fuse.rules.dto.KnowledgeBaseDTO;
+import com.eurodyn.qlack.fuse.rules.mapper.KnowledgeBaseMapper;
+import com.eurodyn.qlack.fuse.rules.model.KnowledgeBase;
+import com.eurodyn.qlack.fuse.rules.model.KnowledgeBaseLibrary;
+import com.eurodyn.qlack.fuse.rules.model.KnowledgeBaseRule;
+import com.eurodyn.qlack.fuse.rules.repository.KnowledgeBaseRepository;
+import com.eurodyn.qlack.fuse.rules.util.RulesUtil;
+import lombok.extern.java.Log;
+import org.kie.api.KieBase;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+/**
+ * This service provides methods related to the KnowledgeBase, KnowledgeBaseLibrary and KnowledgeBaseRule objects.
+ *
+ * @author European Dynamics SA
+ */
+@Service
+@Transactional
+@Log
+public class KnowledgeBaseServiceDupli {
+
+    private final RulesUtil rulesUtil = new RulesUtil();
+
+    private final KnowledgeBaseMapper knowledgeBaseMapper;
+
+    private final KnowledgeBaseRepository knowledgeBaseRepository;
+
+    public KnowledgeBaseServiceDupli(KnowledgeBaseMapper knowledgeBaseMapper, KnowledgeBaseRepository knowledgeBaseRepository) {
+        this.knowledgeBaseMapper = knowledgeBaseMapper;
+        this.knowledgeBaseRepository = knowledgeBaseRepository;
+    }
+
+    /**
+     * Finds the persisted Knowledge Base State based on its id.
+     *
+     * @param knowledgeBaseId the id of the persisted Knowledge Base
+     * @return the persisted Knowledge Base
+     */
+    public KnowledgeBase findKnowledgeBaseStateById(String knowledgeBaseId) {
+        log.info("Retrieving Knowledge Base with id " + knowledgeBaseId);
+
+        KnowledgeBase base = knowledgeBaseRepository.fetchById(knowledgeBaseId);
+
+        if (base == null) {
+            String errorMessage = "Cannot find Knowledge Base with id " + knowledgeBaseId;
+
+            log.severe(errorMessage);
+            throw new QDoesNotExistException(errorMessage);
+        }
+
+        return base;
+    }
+
+    /**
+     * Creates a new Knowledge Base and its linked Knowledge Base Libraries with given libraries and rules.
+     *
+     * @param libraries the libraries of the base
+     * @param rules the rules of the base
+     * @return the id of the newly created Knowledge Base
+     */
+    public String createKnowledgeBase(List<byte[]> libraries, List<String> rules) {
+        log.info("Creating new Knowledge Base with " + libraries.size() + " libraries and " + rules.size() + " rules.");
+
+        KieBase kieBase = rulesUtil.createKieBase(libraries, rules);
+
+        KnowledgeBase knowledgeBase = new KnowledgeBase();
+        knowledgeBase.setState(rulesUtil.serializeKnowledgeBase(kieBase));
+
+        List<KnowledgeBaseLibrary> knowledgeBaseLibraries = new ArrayList<>();
+        for (byte[] library : libraries) {
+            KnowledgeBaseLibrary knowledgeBaseLibrary = new KnowledgeBaseLibrary();
+            knowledgeBaseLibrary.setLibrary(library);
+
+            knowledgeBaseLibrary.setBase(knowledgeBase);
+            knowledgeBaseLibraries.add(knowledgeBaseLibrary);
+        }
+        knowledgeBase.setLibraries(knowledgeBaseLibraries);
+
+        List<KnowledgeBaseRule> knowledgeBaseRules = new ArrayList<>();
+        for (String rule : rules) {
+            KnowledgeBaseRule knowledgeBaseRule = new KnowledgeBaseRule();
+            knowledgeBaseRule.setRule(rule);
+
+            knowledgeBaseRule.setBase(knowledgeBase);
+            knowledgeBaseRules.add(knowledgeBaseRule);
+        }
+        knowledgeBase.setRules(knowledgeBaseRules);
+
+        knowledgeBaseRepository.save(knowledgeBase);
+
+        return knowledgeBase.getId();
+    }
+
+    /**
+     * Retrieves all the Knowledge Base entries.
+     *
+     * @return a list containing all the found Knowledge Base
+     */
+    public List<KnowledgeBaseDTO> getAllKnowledgeBases() {
+        log.log(Level.INFO, "Retrieving all Knowledge Base entries.");
+
+        List<KnowledgeBase> knowledgeBases = knowledgeBaseRepository.findAll();
+
+        return knowledgeBaseMapper.mapToDTO(knowledgeBases);
+    }
+
+}
---
+++ QLACK-Fuse/qlack-fuse-rules/src/main/java/com/eurodyn/qlack/fuse/rules/service/KnowledgeSessionServiceDupli.java
@@ -0,0 +1,208 @@
+package com.eurodyn.qlack.fuse.rules.service;
+
+import com.eurodyn.qlack.fuse.rules.component.RulesComponent;
+import com.eurodyn.qlack.fuse.rules.dto.ExecutionResultsDTO;
+import com.eurodyn.qlack.fuse.rules.model.KnowledgeBase;
+import com.eurodyn.qlack.fuse.rules.util.ClassLoaderKnowledgeBase;
+import com.eurodyn.qlack.fuse.rules.util.JarClassLoaderBuilder;
+import com.eurodyn.qlack.fuse.rules.util.RulesUtil;
+import lombok.extern.java.Log;
+import org.drools.core.base.RuleNameEqualsAgendaFilter;
+import org.drools.core.command.runtime.rule.FireAllRulesCommand;
+import org.kie.api.KieBase;
+import org.kie.api.command.Command;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.StatelessKieSession;
+import org.kie.api.runtime.rule.AgendaFilter;
+import org.kie.internal.command.CommandFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * This service provides methods related to the Kie Session.
+ *
+ * @author European Dynamics SA
+ */
+@Service
+@Transactional
+@Log
+public class KnowledgeSessionServiceDupli {
+
+    private final RulesUtil rulesUtil = new RulesUtil();
+
+    private final RulesComponent rulesComponent;
+
+    private final KnowledgeBaseService knowledgeBaseService;
+
+    public KnowledgeSessionServiceDupli(RulesComponent rulesComponent,
+        KnowledgeBaseService knowledgeBaseService) {
+        this.rulesComponent = rulesComponent;
+        this.knowledgeBaseService = knowledgeBaseService;
+    }
+
+    /**
+     * Creates a new stateful Knowledge Session using an existing Knowledge Base.
+     *
+     * @param knowledgeBaseId the id of the existing Knowledge Base
+     * @return the id of the newly created Knowledge Session
+     */
+    public KieSession createKnowledgeSession(String knowledgeBaseId) {
+        log.info("Creating new Knowledge Session for Knowledge Base with id " + knowledgeBaseId);
+
+        KnowledgeBase knowledgeBaseState = knowledgeBaseService.findKnowledgeBaseStateById(knowledgeBaseId);
+
+        KieBase kieBase = rulesUtil.createKieBaseFromBaseState(knowledgeBaseState).knowledgeBase;
+
+        return kieBase.newKieSession();
+    }
+
+    /**
+     * Execute rules directly using a stateless session. The rules and the libraries can be retrieved from an existing Knowledge Base or can
+     * be given as parameter.
+     *
+     * @param knowledgeBaseId the id of the Knowledge Base that included the libraries and the rules to be executed
+     * @param inputLibraries the libraries to be used
+     * @param inputRules the rules to be executed
+     * @param inputGlobals the globals to be added in the session
+     * @param inputFacts the objects that the rules will be executed against
+     * @param ruleNameToExecute the name of a specific rule that can be executed from the Knowledge Base, while the others are ignored
+     * @return the results of the execution
+     */
+    public ExecutionResultsDTO statelessExecute(String knowledgeBaseId, List<byte[]> inputLibraries, List<String> inputRules,
+        Map<String, byte[]> inputGlobals, List<byte[]> inputFacts, String ruleNameToExecute) {
+        log.info("Creating Stateless Knowledge Session using the Knowledge Base with id " + knowledgeBaseId);
+
+        KieBase kieBase;
+        ClassLoader classLoader;
+        List<Command<?>> commands = new ArrayList<>();
+
+        if (knowledgeBaseId != null) {
+            KnowledgeBase knowledgeBaseState = knowledgeBaseService.findKnowledgeBaseStateById(knowledgeBaseId);
+            ClassLoaderKnowledgeBase clkb = rulesUtil.createKieBaseFromBaseState(knowledgeBaseState);
+            kieBase = clkb.knowledgeBase;
+
+            classLoader = clkb.classLoader;
+        } else {
+            kieBase = rulesUtil.createKieBase(inputLibraries, inputRules);
+
+            JarClassLoaderBuilder classLoaderBuilder = new JarClassLoaderBuilder();
+            classLoader = classLoaderBuilder.buildClassLoader(null);
+        }
+
+        //set globals
+        Map<String, Object> globals = new LinkedHashMap<>();
+        if (inputGlobals != null) {
+            for (Entry<String, byte[]> inputGlobal : inputGlobals.entrySet()) {
+                String id = inputGlobal.getKey();
+                Object object = rulesComponent.deserializeObject(classLoader, inputGlobal.getValue());
+                globals.put(id, object);
+            }
+            globals.entrySet().stream().forEach(g -> commands.add(CommandFactory.newSetGlobal(g.getKey(), g.getValue())));
+        }
+
+        //set facts
+        List<Object> facts = new ArrayList<>();
+        try {
+            for (byte[] inputFact : inputFacts) {
+                Object object = rulesComponent.deserializeObject(classLoader, inputFact);
+                facts.add(object);
+            }
+            facts.stream().forEach(f -> commands.add(CommandFactory.newInsert(f)));
+        } catch (NullPointerException e) {
+            log.severe("Cannot execute session without facts. At least one fact must be provided.");
+            return null;
+        }
+
+        //set rules
+        if ((ruleNameToExecute != null) && (knowledgeBaseId != null)) {
+            AgendaFilter ruleNameFilter = new RuleNameEqualsAgendaFilter(ruleNameToExecute);
+            commands.add(new FireAllRulesCommand(ruleNameFilter));
+        }
+
+        //execute
+        StatelessKieSession statelessKieSession = kieBase.newStatelessKieSession();
+        statelessKieSession.execute(CommandFactory.newBatchExecution(commands));
+
+        //get global results
+        return getResults(globals, facts);
+    }
+
+    /**
+     * Execute the rules of an existing Knowledge Base.
+     *
+     * @param knowledgeBaseId the id of the Knowledge Base
+     * @param rules a list containing the names of the rules that can be executed from the Knowledge Base, while the others are ignored
+     * @param inputGlobals the globals to be added in the session
+     * @param inputFacts the objects that the rules will be executed against
+     * @return the results of the execution
+     */
+    public ExecutionResultsDTO fireRules(String knowledgeBaseId, List<String> rules, Map<String, byte[]> inputGlobals,
+        List<byte[]> inputFacts) {
+        log.info("Firing rules of the Knowledge Base with id " + knowledgeBaseId);
+
+        KnowledgeBase knowledgeBaseState = knowledgeBaseService.findKnowledgeBaseStateById(knowledgeBaseId);
+
+        ClassLoaderKnowledgeBase clkb = rulesUtil.createKieBaseFromBaseState(knowledgeBaseState);
+
+        KieSession kieSession = clkb.knowledgeBase.newKieSession();
+        ClassLoader classLoader = clkb.classLoader;
+
+        //set globals
+        Map<String, Object> globals = new LinkedHashMap<>();
+        if (inputGlobals != null) {
+            for (Entry<String, byte[]> inputGlobal : inputGlobals.entrySet()) {
+                globals.put(inputGlobal.getKey(), rulesComponent.deserializeObject(classLoader, inputGlobal.getValue()));
+            }
+            globals.entrySet().stream().forEach(g -> kieSession.setGlobal(g.getKey(), g.getValue()));
+        }
+
+        //set facts
+        List<Object> facts = new ArrayList<>();
+        if (inputFacts != null) {
+            for (byte[] inputFact : inputFacts) {
+                facts.add(rulesComponent.deserializeObject(classLoader, inputFact));
+            }
+            facts.stream().forEach(f -> kieSession.insert(f));
+        }
+
+        //set rules
+        if (rules == null) {
+            kieSession.fireAllRules();
+        } else {
+            AgendaFilter filter = match -> {
+                String matchedRule = match.getRule().getName();
+                return rules.contains(matchedRule);
+            };
+            kieSession.fireAllRules(filter);
+        }
+
+        kieSession.dispose();
+
+        return getResults(globals, facts);
+    }
+
+    private ExecutionResultsDTO getResults(Map<String, Object> globals, List<Object> facts) {
+        //get global results
+        Map<String, byte[]> outputGlobals = new LinkedHashMap<>();
+        for (Entry<String, Object> object : globals.entrySet()) {
+            String id = object.getKey();
+            byte[] bytes = rulesComponent.serializeObject(object.getValue());
+            outputGlobals.put(id, bytes);
+        }
+
+        //get facts results
+        List<byte[]> outputFacts = new ArrayList<>();
+        for (Object object : facts) {
+            byte[] bytes = rulesComponent.serializeObject(object);
+            outputFacts.add(bytes);
+        }
+
+        return new ExecutionResultsDTO(outputGlobals, outputFacts);
+    }
+}
