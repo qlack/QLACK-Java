2cfc270478b90bf75756dcc483614720b258b4c3
atsatsou <mahi.tsatsou@eurodyn.com>
2019-10-21 12:05:37 +0300

    added unit tests in qlack-fuse-aaa module

---
+++ QLACK-Fuse/qlack-fuse-aaa/src/test/java/com/eurodyn/qlack/fuse/aaa/mapper/AAAMapperTest.java
@@ -0,0 +1,69 @@
+package com.eurodyn.qlack.fuse.aaa.mapper;
+
+import com.eurodyn.qlack.fuse.aaa.InitTestValues;
+import com.eurodyn.qlack.fuse.aaa.dto.BaseDTO;
+import com.eurodyn.qlack.fuse.aaa.model.AAAModel;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertNotNull;
+
+/**
+ * @author European Dynamics
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class AAAMapperTest {
+
+    private AAAMapper aaaMapper = new AAAMapper() {
+        @Override
+        public BaseDTO mapToDTO(AAAModel entity) {
+            return null;
+        }
+
+        @Override
+        public List mapToDTO(List entity) {
+            return null;
+        }
+
+        @Override
+        public AAAModel mapToEntity(BaseDTO dto) {
+            return null;
+        }
+
+        @Override
+        public void mapToExistingEntity(BaseDTO dto, AAAModel entity) {
+
+        }
+
+        @Override
+        public List mapToEntity(List dto) {
+            return null;
+        }
+    };
+
+    @Mock
+    Page<AAAModel> aaaModel;
+
+
+    private InitTestValues initTestValues;
+
+    @Before
+    public void init() {
+        initTestValues = new InitTestValues();
+    }
+
+    @Test
+    public void mapTest() {
+        aaaMapper.map(aaaModel);
+        assertNotNull(aaaModel);
+    }
+
+
+}
