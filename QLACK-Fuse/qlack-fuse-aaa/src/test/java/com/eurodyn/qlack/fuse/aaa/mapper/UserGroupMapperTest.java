c8162d51af799ac939f75814e4335df19b038bef
atsatsou <mahi.tsatsou@eurodyn.com>
2019-10-23 11:39:39 +0300

    added unit tests in qlack-fuse-aaa module

--- QLACK-Fuse/qlack-fuse-aaa/src/test/java/com/eurodyn/qlack/fuse/aaa/mapper/UserGroupMapperTest.java
+++ QLACK-Fuse/qlack-fuse-aaa/src/test/java/com/eurodyn/qlack/fuse/aaa/mapper/UserGroupMapperTest.java
@@ -1,10 +1,14 @@
 package com.eurodyn.qlack.fuse.aaa.mapper;
 
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import com.eurodyn.qlack.fuse.aaa.InitTestValues;
 import com.eurodyn.qlack.fuse.aaa.dto.UserGroupDTO;
 import com.eurodyn.qlack.fuse.aaa.model.UserGroup;
+
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
@@ -146,4 +150,89 @@ public class UserGroupMapperTest {
     List<UserGroup> userGroups = userGroupMapperImpl.mapToEntity(userGroupsDTO);
     assertEquals(userGroupsDTO.size(), userGroups.size());
   }
+
+  @Test
+  public void mapToDTONullTest(){
+    userGroupDTO = userGroupMapperImpl.mapToDTO((UserGroup) null, false);
+    assertEquals(null,userGroupDTO);
+  }
+
+  @Test
+  public void mapToDTOListNullTest(){
+    userGroupsDTO = userGroupMapperImpl.mapToDTO((List<UserGroup>) null,false);
+    assertEquals(null,userGroupsDTO);
+
+  }
+
+  @Test
+  public void mapToEntityNullTest() {
+    userGroup = userGroupMapperImpl.mapToEntity((UserGroupDTO) null);
+    assertEquals(null, userGroup);
+
+  }
+  @Test
+  public void mapListToListEntityNullTest(){
+      userGroups = userGroupMapperImpl.mapToEntity((List<UserGroupDTO>) null);
+      assertEquals(null,userGroups);
+    }
+
+  @Test
+  public void userGroupParentIdTest(){
+    userGroup.getParent().setId(null);
+    userGroupDTO = userGroupMapperImpl.mapToDTO(userGroup, false);
+    assertEquals(userGroup.getParent().getId(),userGroupDTO.getParentId());
+    }
+
+  @Test
+  public void userGroupDTOToUserGroupTest(){
+    userGroup = userGroupMapperImpl.userGroupDTOToUserGroup(null);
+    assertEquals(null,userGroup);
+   }
+
+  @Test
+  public void mapToExistingEntityNullTest(){
+   UserGroupDTO userGroupDTO = initTestValues.createUserGroupDTO();
+   userGroupDTO.setName("updated name");
+   userGroupMapperImpl.mapToExistingEntity(userGroupDTO, userGroup);
+   assertEquals(userGroupDTO.getName(), userGroup.getName());
+ }
+
+  @Test
+  public void mapToExistingChildrenTest(){
+    UserGroupDTO userGroupDTO = initTestValues.createUserGroupDTO();
+    userGroupDTO.setChildren(null);
+    userGroupMapperImpl.mapToExistingEntity(userGroupDTO,userGroup);
+    assertEquals(userGroupDTO.getChildren(),userGroup.getChildren());
+    }
+
+  @Test
+  public void mapToExistingEntityUserGroupDTONull(){
+   UserGroup userGroup = initTestValues.createUserGroup();
+   userGroup.setName(null);
+   userGroupMapperImpl.mapToExistingEntity(null,userGroup);
+   assertEquals(userGroup.getName(),userGroupDTO.getName());
+  }
+
+  @Test
+  public void userGroupDTOSetToUserGroupListTest(){
+    UserGroup userGroup = initTestValues.createUserGroup();
+    List<UserGroup> userGroups = initTestValues.createUserGroups();
+    userGroup.setChildren(null);
+    userGroupMapperImpl.mapToExistingEntity(userGroupDTO,userGroup);
+    userGroups =  userGroupMapperImpl.userGroupDTOSetToUserGroupList(null);
+    assertEquals(null,userGroups);
+  }
+
+  @Test
+  public void mapToExistingEntityNullListTest(){
+    UserGroup userGroup = initTestValues.createUserGroup();
+    userGroup.setChildren(null);
+    assertNull(userGroup.getChildren());
+    UserGroupDTO userGroupDTO = initTestValues.createUserGroupDTO();
+    userGroupDTO.setChildren(null);
+    assertNull(userGroupDTO.getChildren());
+    userGroupMapperImpl.mapToExistingEntity(userGroupDTO,userGroup);
+  }
+
+
 }
