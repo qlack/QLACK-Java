d8c52b0eb74e1f6504ca7a0ef6cb7b44342db7c3
Nassos Michas <nassosmichas@me.com>
2020-07-13 15:51:58 +0300

    Printable JWT

--- QLACK-Util/qlack-util-jwt/src/main/java/com/eurodyn/qlack/util/jwt/dto/JwtDTO.java
+++ QLACK-Util/qlack-util-jwt/src/main/java/com/eurodyn/qlack/util/jwt/dto/JwtDTO.java
@@ -3,6 +3,7 @@ package com.eurodyn.qlack.util.jwt.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 /**
  * A placeholder for an encoded JWT.
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@ToString
 public class JwtDTO {
 
   private String jwt;
